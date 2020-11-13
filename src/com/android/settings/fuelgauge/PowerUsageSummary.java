Kingsman44/platform_packages_apps_Settings

Settings: Battery temp: Use MCC to determine the scale type
The whole tapping on the preference was cute but really not ideal. Most
users are one or the other and is usually based on where the hell they're
at. Using the MCC does just that.

Change-Id: I048cda9032a817035c32087cfd9ffeb49003d9d1

Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -371,7 +371,9 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         mScreenUsagePref.setSummary(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
         mBatteryTemp.setSummary(
-                com.android.internal.util.octavi.OctaviUtils.batteryTemperature(getContext(), false));
+                com.android.internal.util.octavi.OctaviUtils.mccCheck(getContext()) ?
+                com.android.internal.util.octavi.OctaviUtils.batteryTemperature(getContext(), true) + "°F" :
+                com.android.internal.util.octavi.OctaviUtils.batteryTemperature(getContext(), false) + "°C");
 
         final long elapsedRealtimeUs = SystemClock.elapsedRealtime() * 1000;
         Intent batteryBroadcast = context.registerReceiver(null,

ziasam/android_packages_apps_Settings

src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -370,7 +370,9 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         mScreenUsagePref.setSummary(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
         mBatteryTemp.setSummary(
-                com.android.internal.util.du.Utils.batteryTemperature(getContext(), false));
+                com.android.internal.util.du.Utils.mccCheck(getContext()) ?
+                com.android.internal.util.du.Utils.batteryTemperature(getContext(), true) + "°F" :
+                com.android.internal.util.du.Utils.batteryTemperature(getContext(), false) + "°C");
 
         final long elapsedRealtimeUs = SystemClock.elapsedRealtime() * 1000;
         Intent batteryBroadcast = context.registerReceiver(null,