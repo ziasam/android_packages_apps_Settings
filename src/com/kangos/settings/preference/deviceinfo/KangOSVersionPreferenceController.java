22949cbbdb366cc2d9ee66b52181120c67df00b4
elluzion <dyrex2004@gmail.com>
2020-12-31 00:10:03 +0100

    AboutInfo:Revamp KangOS Version
    Required Flags in DT:
    PRODUCT_PRODUCT_PROPERTIES += \
      ro.kangos.maintainer=maintainername \
      ro.kangos.cpu=for example SDM855
    
    Co-authored-by: Androbuddy <anirudhchimpidi8@gmail.com>
    Co-authored-by: moditjain9777 <moditjain121@gmail.com>

--- src/com/kangos/settings/preference/deviceinfo/KangOSVersionPreferenceController.java
+++ src/com/kangos/settings/preference/deviceinfo/KangOSVersionPreferenceController.java
@@ -17,29 +17,119 @@
 
 package com.kangos.settings.preference.deviceinfo;
 
+import java.io.IOException;
 import android.content.Context;
+import androidx.annotation.VisibleForTesting;
 import android.os.SystemProperties;
 
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+import com.android.settings.utils.KangOSSpecUtils;
+
 import com.android.settings.core.BasePreferenceController;
+import com.android.settingslib.widget.LayoutPreference;
+import com.android.settingslib.Utils;
+import com.android.settings.core.PreferenceControllerMixin;
+
+import android.widget.TextView;
 import com.android.settings.R;
 
 public class KangOSVersionPreferenceController extends BasePreferenceController {
 
-    private final String KEY_KANGOS_VERSION_PROP = "ro.kangos.version";
- 
-    public KangOSVersionPreferenceController(Context context,
-            String preferenceKey) {
+    @VisibleForTesting
+    private static final String KEY_KANGOS_VERSION_PROP = "ro.kangos.version";
+
+    @VisibleForTesting
+    TextView mKangOSVersionText;
+    @VisibleForTesting
+    TextView mKangOSVersionFlavourText;
+    @VisibleForTesting
+    TextView mDeviceNameText;
+    @VisibleForTesting
+    TextView mCpuText;
+    @VisibleForTesting
+    TextView mScreenResText;
+    @VisibleForTesting
+    TextView mBatteryText;
+    @VisibleForTesting
+    TextView mRamText;
+    @VisibleForTesting
+    TextView mMaintainerText;
+
+    private PreferenceFragmentCompat mHost;
+    private LayoutPreference mKangOSVersionLayoutPref;
+    private Context mContext;
+
+    public KangOSVersionPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
+        mContext = context;
     }
 
     @Override
     public int getAvailabilityStatus() {
-        return AVAILABLE_UNSEARCHABLE;
+        return AVAILABLE;
+    }
+
+    public void setFragment(PreferenceFragmentCompat fragment) {
+        mHost = fragment;
     }
 
     @Override
-    public CharSequence getSummary() {
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mKangOSVersionLayoutPref = screen.findPreference(getPreferenceKey());
+        mKangOSVersionText = mKangOSVersionLayoutPref.findViewById(R.id.kangos_version);
+        mKangOSVersionFlavourText = mKangOSVersionLayoutPref.findViewById(R.id.kangos_version_flavour);
+        mDeviceNameText = mKangOSVersionLayoutPref.findViewById(R.id.device_name_text);
+        mCpuText = mKangOSVersionLayoutPref.findViewById(R.id.device_cpu_text);
+        mScreenResText = mKangOSVersionLayoutPref.findViewById(R.id.device_screen_res_text);
+        mBatteryText = mKangOSVersionLayoutPref.findViewById(R.id.device_battery_text);
+        mRamText = mKangOSVersionLayoutPref.findViewById(R.id.device_ram_text);
+        mMaintainerText = mKangOSVersionLayoutPref.findViewById(R.id.device_maintainer_text);
+
+        UpdateKangOSVersionPreference();
+    }
+
+    private void UpdateKangOSVersionPreference() {
+        // We split the different specs into different voids to make the code more organized.
+        updateKangOSVersionText();
+        updateDeviceNameText();
+        updateCpuText();
+        updateScreenResText();
+        updateBatteryText();
+        updateRamText();
+        updateMaintainerText();
+
+    }
+
+    public CharSequence updateKangOSVersionText() {
         return SystemProperties.get(KEY_KANGOS_VERSION_PROP,
                 mContext.getString(R.string.unknown));
+
     }
+
+    private void updateDeviceNameText() {
+        mDeviceNameText.setText(KangOSSpecUtils.getDeviceName());
+    }
+
+    private void updateBatteryText() {
+        mBatteryText.setText(KangOSSpecUtils.getBatteryCapacity(mContext) + " mAh");
+    }
+
+    private void updateCpuText() {
+        mCpuText.setText(KangOSSpecUtils.getProcessorModel());
+    }
+
+    private void updateScreenResText() {
+        mScreenResText.setText(KangOSSpecUtils.getScreenRes(mContext));
+    }
+
+    private void updateRamText() {
+        mRamText.setText(String.valueOf(KangOSSpecUtils.getTotalRAM())+ " GB");
+    }
+    private void updateMaintainerText() {
+        mMaintainerText.setText(String.valueOf(KangOSSpecUtils.getMaintainerName()));
+    }
+
 }
+
